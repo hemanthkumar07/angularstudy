d1352141ad416988abf723860b62f2df15fb1b9a
HEMANTH KUMAR HARI <hemanthkumarhari@HEMANTHs-MacBook-Pro.local>
2022-11-28 13:04:18 +0530

    hey

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -5,10 +5,19 @@ import { HttpClient } from '@angular/common/http'
   providedIn: 'root'
 })
 export class AuthService {
-  api_url="http://localhost:3000/post"
+  api_url="http://localhost:3000/post/"
 
   constructor( private http:HttpClient) { }
   getEmployee(){
     return this.http.get(this.api_url)
+  }
+  postEmployee(data1:any){
+    return this.http.post(this.api_url,data1)
+  }
+  deleteEmployee(id:any){
+    return this.http.delete(this.api_url+id)
+  }
+  editEmployee(data2:any,id:any){
+    return this.http.put(this.api_url+id,data2)
   }
 }
--- src/app/employeddetail/employeddetail.component.ts
+++ src/app/employeddetail/employeddetail.component.ts
@@ -10,7 +10,7 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./employeddetail.component.css']
 })
 export class EmployeddetailComponent implements OnInit {
-  emp1!:Employeedet
+  emp1:Employeedet=new Employeedet()
 
   empdata:any
 
@@ -29,8 +29,16 @@ export class EmployeddetailComponent implements OnInit {
     
   }
   oncreate(emp:NgForm){
-    console.log(emp.value)
+    // this.emp1.first_name=emp.value.first_name
+    // this.emp1.last_name=emp.value.last_name
+    // this.emp1.email_id=emp.value.email_id
+
+    this.auth.postEmployee(this.emp1).subscribe((data)=>{
+      console.log(data)
+    })
+    
     emp.reset()
+    this.getemp()
   }
   getemp(){
     this.auth.getEmployee().subscribe(data =>{
@@ -39,7 +47,41 @@ export class EmployeddetailComponent implements OnInit {
     })
 
   }
+  ondelete(id:any){
+   
+    this.auth.deleteEmployee(id).subscribe(data =>{
+      console.log(data)
+      this.getemp()
+    })
+  }
+  onedit(block:any){
 
+    this.emp1={
+      id:block.id,
+      first_name:block.first_name,
+      last_name:block.last_name,
+      email_id:block.email_id
+    }
+    
+  }
+
+  onupdate(b:any){
+    
+    // this.emp1.first_name=b.value.first_name
+    // this.emp1.last_name=b.value.last_name
+    // this.emp1.email_id=b.value.email_id
+    this.auth.editEmployee(this.emp1,this.emp1.id).subscribe((data) =>{
+      console.log(data)
+      b.reset()
+      this.getemp()
+      
+    })
+  }
+
+  
+
+    
+  
 }
 export class Employeedet{
   id:number=0;
